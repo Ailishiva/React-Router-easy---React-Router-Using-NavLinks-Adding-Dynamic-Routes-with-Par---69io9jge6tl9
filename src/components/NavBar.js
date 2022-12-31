e4c5c8f8553765efcd0bd7a85a41a202392fb576
Aili shivaprasad <aili.shivaprasad@gmail.com>
2022-12-31 19:41:09 +0530

    commited

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,22 @@
 import React from 'react';
-
-
+import { Link, NavLink } from 'react-router-dom';
+import { Index } from '../Pages/Index';
+import { Home } from '../Pages/Home';
 export const NavBar = () =>{
     return(
+<nav>
 
+        <NavLink to="/">{<Index/>}</NavLink>
+        <NavLink
+         to="/home"
+         activeClassName="selectedLink">
+          {<Home/>}
+        </NavLink>
+</nav>
     )
 }
+    {/* <NavLink>
+        <Link className='index-link' to={"/"}><Index/></Link>
+        <Link className='home-link' to={"/home"}><Home/></Link>
+    </NavLink> */}
+    
