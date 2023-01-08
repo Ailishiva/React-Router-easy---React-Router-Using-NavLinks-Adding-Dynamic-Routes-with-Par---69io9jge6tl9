76c4c87fff3124c531db83a5a190a28345631131
Aili ShivaPrasad <112164679+Ailishiva@users.noreply.github.com>
2023-01-08 19:55:29 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import { Route, Routes} from "react-router-dom"
-import { Index } from './Pages/Index';
-import { Home } from './Pages/Home';
-import { NotFound } from './Pages/NotFound';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Index } from "./Pages/Index";
+import { NotFound } from "./Pages/NotFound";
+
 export const AppRoutes = () => {
-    return (
-<<<<<<< HEAD
-=======
-        
->>>>>>> 0f3bc77603bdc2a4c931fdd5bee53f0db8765b54
-        <Routes>
-        <Route path="/" element={<Index/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="*" element={<NotFound/>}/>
-        </Routes>
-<<<<<<< HEAD
-=======
-       
->>>>>>> 0f3bc77603bdc2a4c931fdd5bee53f0db8765b54
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
