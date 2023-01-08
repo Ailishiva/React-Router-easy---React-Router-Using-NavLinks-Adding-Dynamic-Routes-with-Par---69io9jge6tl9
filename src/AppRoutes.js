1b970cde351d50515314679cb175f54a667c3a2e
Aili ShivaPrasad <112164679+Ailishiva@users.noreply.github.com>
2023-01-08 19:28:54 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -5,12 +5,12 @@ import { Home } from './Pages/Home';
 import { NotFound } from './Pages/NotFound';
 export const AppRoutes = () => {
     return (
-        <>
+        
         <Routes>
         <Route path="/" element={<Index/>}/>
-        <Route path="home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
         <Route path="*" element={<NotFound/>}/>
         </Routes>
-       </>
+       
     )
 }
